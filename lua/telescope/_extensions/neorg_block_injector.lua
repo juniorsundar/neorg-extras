30ac6e8e2652155dbed76b956e2fad035dbf1d17
Junior Sundar <juniorsundar@gmail.com>
2024-08-20 22:42:41 +0400

    refactor: Migrated roam as external

--- lua/telescope/_extensions/neorg_block_injector.lua
+++
@@ -1,7 +0,0 @@
-local telescopic = require("neorg-extras.telescopic")
-
-return require("telescope").register_extension({
-    exports = {
-        neorg_block_injector = telescopic.neorg_block_injector,
-    }
-})
