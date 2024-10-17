2bc8edf33c759279515352a0fd1475a194c784fa
Junior Sundar <juniorsundar@gmail.com>
2024-10-17 23:50:49 +0400

    fix(buff-man): Clean agenda view window upon quit

--- lua/neorg/modules/external/many-mans/module.lua
+++ lua/neorg/modules/external/many-mans/module.lua
@@ -743,6 +743,7 @@ module.public = {
 				vim.api.nvim_set_option_value(opt[1], opt[2], { win = module.public["buff-man"].win })
 			end
 			vim.api.nvim_buf_delete(module.public["buff-man"].buf, { force = true })
+            module.public["buff-man"].win = nil
 		end,
 
 		open_to_target_backlink = function()
@@ -810,6 +811,7 @@ module.public = {
 						vim.api.nvim_set_option_value(opt[1], opt[2], { win = module.public["buff-man"].win })
 					end
 					vim.api.nvim_buf_delete(module.public["buff-man"].buf, { force = true })
+					module.public["buff-man"].win = nil
 				end,
 			})
 
