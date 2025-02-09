c22813248465c3056a3c05ae639c7801e05f4208
juniorsundar <24962525+juniorsundar@users.noreply.github.com>
2025-02-09 15:42:32 +0000

    chore: autoformat with stylua
    chore(main): release 0.7.0 (#52)
    
    chore: autoformat with stylua
    
    doc: Update README.md
    
    chore: Auto-generate docs
    
    chore(main): release 0.7.0 (#55)

--- lua/neorg/modules/external/roam/module.lua
+++ lua/neorg/modules/external/roam/module.lua
@@ -747,15 +747,12 @@ module.public = {
 					local file, lineno, text = line:match(pattern)
 					local metadata = module.required["external.many-mans"]["meta-man"].extract_file_metadata(file)
 					if metadata ~= nil then
-						table.insert(
-							items,
-							{
-								text = metadata["title"] .. " | " .. text,
-								heading = text,
-								file = file,
-								pos = { tonumber(lineno), 1 },
-							}
-						)
+						table.insert(items, {
+							text = metadata["title"] .. " | " .. text,
+							heading = text,
+							file = file,
+							pos = { tonumber(lineno), 1 },
+						})
 					else
 						table.insert(
 							items,
@@ -1138,10 +1135,11 @@ module.public = {
 								{ text = "Untitled @" .. lineno, file = file, pos = { tonumber(lineno), 1 } }
 							)
 						elseif metadata["title"] ~= self_title then
-							table.insert(
-								items,
-								{ text = metadata["title"] .. " @" .. lineno, file = file, pos = { tonumber(lineno), 1 } }
-							)
+							table.insert(items, {
+								text = metadata["title"] .. " @" .. lineno,
+								file = file,
+								pos = { tonumber(lineno), 1 },
+							})
 						else
 							table.insert(
 								items,
